92aaf39200ed894fc118e7057251b9f554800e2a
kkpp9989 <117346116+kkpp9989@users.noreply.github.com>
2026-05-26 16:56:33 +0700

    Rename package from 'SharedFrameworks' to 'AmitySDK'

--- Package.swift
+++ Package.swift
@@ -4,18 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "SharedFrameworks",
+    name: "AmitySDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SharedFrameworks",
-            targets: ["SharedFrameworks", "AmitySDK", "Realm", "RealmSwift", "AmityLiveVideoBroadcastKit", "AmityVideoPlayerKit", "MobileVLCKit"]),
+            name: "AmitySDK",
+            targets: ["AmitySDK", "Realm", "RealmSwift", "AmityLiveVideoBroadcastKit", "AmityVideoPlayerKit", "MobileVLCKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
+targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
